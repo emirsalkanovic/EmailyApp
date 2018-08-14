f814a4f714de42c6b21452a566bb1017949c4ddc
Emir Salkanovic <emirsalkanovic1@gamil.com>
2018-08-14 21:14:34 +0200

    Adding billing and client side app!

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import materializeCSS from 'materialize-css/dist/css/materialize.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore( reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+	<Provider store={store}><App /></Provider>,
+	 document.querySelector("#root"));
+
+	 console.log('STRIPE KEY IS', process.env.REACT_APP_STRIPE_KEY);
+	 console.log('envoirment is', process.env.NODE_ENV);
--- services/passport.js
+++ services/passport.js
@@ -21,17 +21,18 @@ passport.use(new GoogleStrategy({
 	clientSecret: keys.googleClientSecret,
 	callbackURL: '/auth/google/callback',
 	proxy: true
-}, (accessToken, refreshToken, profile, done) =>{
-	User.findOne({ googleId: profile.id })
-		.then((existingUser) => {
+}, async (accessToken, refreshToken, profile, done) =>{
+	const existingUser = await User.findOne({ googleId: profile.id })
+		
 			if(existingUser) {
 				//User postoji i ne treba ga praviti ponovo
 				done(null, existingUser);
-			}else {
-				new User({ googleId: profile.id }).save()
-					.then(user => done(null, user));
 			}
-		});
+			{
+				const user = await new User({ googleId: profile.id }).save()
+					done(null, user);
+			}
+	
 
 	
 })
